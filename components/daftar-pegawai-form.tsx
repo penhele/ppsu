0021c7bf9c21a12e06e00cbdc06bc38148d0097d
penhele <stephenhelk@gmail.com>
2025-10-24 21:01:35 +0700

    refactor: change (app) to (admin)

--- components/daftar-pegawai-form.tsx
+++ components/daftar-pegawai-form.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import CreateDaftarPegawaiForm from "./create-daftar-pegawai-form";
 
 const DaftarPegawaiForm = () => {
--- components/input.tsx
+++ components/input.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { Label } from "./ui/label";
-import { Input } from "./ui/input";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectContent,
@@ -8,7 +7,7 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "./ui/select";
+} from "@/components/ui/select";
 import { formatCapital } from "@/lib/utils";
 
 export const InputText = ({
