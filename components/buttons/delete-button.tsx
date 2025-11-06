3f343d7710908564ed2e0d980064b4464e490551
Stephen Helenus <stephenhelk@gmail.com>
2025-11-06 14:07:44 +0700

    refactor: fix bug delete button

--- components/buttons/delete-button.tsx
+++ components/buttons/delete-button.tsx
@@ -1,18 +1,36 @@
 import { Button } from "@/components/ui/button";
 import { deletePegawaiById } from "@/lib/action/pegawai";
 import { Trash } from "lucide-react";
+import { FormEvent, useTransition } from "react";
+import { toast } from "sonner";
+import { Spinner } from "@/components/ui/spinner";
 
 export const DeleteButton = ({ id }: { id: string }) => {
-  const deleteById = deletePegawaiById.bind(null, id);
+  const [isPending, startTransition] = useTransition();
+
+  function handleDelete() {
+    startTransition(async () => {
+      const result = await deletePegawaiById(id);
+
+      if (result.success) {
+        toast.success(result.message);
+      } else {
+        toast.error(result.message);
+      }
+    });
+  }
 
   return (
-    <form action={deleteById}>
-      <Button
-        variant="outline"
-        className="border-red-100 hover:border-red-500 w-8 h-8"
-      >
+    <Button
+      onClick={handleDelete}
+      variant="outline"
+      className="border-red-100 hover:border-red-500 w-8 h-8 flex gap-2 items-center justify-center"
+    >
+      {isPending ? (
+        <Spinner className="text-red-500" />
+      ) : (
         <Trash className="text-red-500" />
-      </Button>
-    </form>
+      )}
+    </Button>
   );
 };
